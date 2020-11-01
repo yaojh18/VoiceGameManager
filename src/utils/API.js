970fe94503361023f60d603328d4c15ffc966bbe
zhangsn-19 <zhang-sn19@mails.tsinghua.edu.cn>
2020-11-01 19:38:00 +0800

    Modification of connecting with backend

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,38 +1,38 @@
 const API = {
     GET_MESSAGE_LIST:{
-        path:"/api/manager/get_list",
+        path:"/api/manager/get_list/",
         method: "get"
     },
     POST_NEW_MESSAGE:{
-        path:"/api/manager/add",
+        path:"/api/manager/add/",
         method: "post"
     },
     ADD: {
-        path: "/api/manager/add",
+        path: "/api/manager/add/",
         method: "post"
     },
     EDIT: {
-        path: "/api/manager/edit",
+        path: "/api/manager/edit/",
         method: "post"
     },
     SEARCH: {
-        path: "/api/manager/search",
+        path: "/api/manager/search/",
         method: "post"
     },
     DELETE: {
-        path: "/api/manager/delete",
+        path: "/api/manager/delete/",
         method: "post"
     },
     LOGIN: {
-        path: "/api/users/login",
+        path: "/api/users/login/",
         method: "post"
     },
     REGISTER: {
-        path: "/api/users/registration",
+        path: "/api/users/registration/",
         method: "post"
     },
     GETLIST: {
-        path: "/api/manager/get_list",
+        path: "/api/manager/get_list/",
         method: "get"
     }
 
