90401910dc6fb63788dba478d17a290abc915886
zhangsn-19 <zhang-sn19@mails.tsinghua.edu.cn>
2020-11-03 08:43:34 +0800

    Modification when connecting some backends

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,8 +1,12 @@
 <template>
   <el-container>
-    <el-card>个人信息修改
-    </el-card>
-    <ModifyPwd />
+    <el-button v-on:click="handleModify()" style="display: inline-block;margin-right: 15px;">
+      <i class="el-icon-edit">个人信息修改</i>
+    </el-button>
+    <el-button v-on:click="this.$router.push({path:'/home'})" style="display: inline-block;margin-right: 15px;">
+      <router-link to='/'><i class="el-icon-s-home">回到主页</i></router-link>
+    </el-button>
+    <ModifyPwd v-bind:dialog-visible="ModifyPwd.dialogVisible"/>
   </el-container>
 </template>
 <script>
@@ -11,6 +15,25 @@ export default {
     name: "Person",
     components: {
       ModifyPwd,
+    },
+    data(){
+      return{
+         ModifyPwd : {
+           dialogVisible: true,
+           form: {
+             username:this.username,
+             password:this.password,
+             password2:this.password2,
+             password3:this.password3,
+           },
+         }
+      }
+    },
+    methods:{
+      handleModify(){
+        this.$router.go('/person');
+        this.ModifyPwd.dialogVisible = true;
+      },
     }
 }
 </script>
