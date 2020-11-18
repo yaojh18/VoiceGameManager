4cafa2de8ef35d184429fa86ce6cf2388a461a3a
zhangsn-19 <zhang-sn19@mails.tsinghua.edu.cn>
2020-11-18 16:33:05 +0800

    进行了CICD的修改:修改api:url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'https://voicetestgame-dijkstra.app.secoder.net', // 修改为你的Django服务器地址
+                target: 'https://VoiceTestGame.Dijkstra.secoder.local', // 修改为你的Django服务器地址
                 changeOrigin: true,
             }
         },
