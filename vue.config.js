0c8f70d35cb5de258a222f85747e956525040ec7
zhangsn-19 <zhang-sn19@mails.tsinghua.edu.cn>
2020-11-18 15:59:57 +0800

    进行了CICD的修改

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const path = require('path')
-const testMode = process.env.NODE_ENV === 'production'
-
 module.exports = {
     devServer: {
         proxy: {
@@ -13,23 +10,4 @@ module.exports = {
     },
     lintOnSave: false,
     productionSourceMap: false,
-
-    chainWebpack: config => {
-
-        if (testMode) {
-            config.merge({
-                devtool: 'eval'
-            })
-            config.module
-                .rule('istanbul')
-                .test(/\.(js|vue)$/)
-                .include
-                .add(path.resolve(__dirname, '/src'))
-                .end()
-                .use('istanbul-instrumenter-loader')
-                .loader('istanbul-instrumenter-loader')
-                .options({ esModules: true })
-                .before('babel-loader')
-        }
-    }
 }
